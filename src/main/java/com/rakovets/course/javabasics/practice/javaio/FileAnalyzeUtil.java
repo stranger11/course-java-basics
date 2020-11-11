f136542d4a185480f4712ef8c668ea1011906ed8
Yan Pashyk <yanislav888@mail.ru>
2020-11-11 15:06:37 +0300

    done tasks 5 - 9 for theme "java IO"

--- src/main/java/com/rakovets/course/javabasics/practice/javaio/FileAnalyzeUtil.java
+++ src/main/java/com/rakovets/course/javabasics/practice/javaio/FileAnalyzeUtil.java
@@ -2,10 +2,11 @@ package com.rakovets.course.javabasics.practice.javaio;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.*;
 
 public class FileAnalyzeUtil {
     public static ArrayList<String> getListStringOfFile(String text) {
@@ -20,7 +21,6 @@ public class FileAnalyzeUtil {
         }
         return list;
     }
-
 
     public static ArrayList<String> getWordsStartWithVowel(String text) {
         String word = "";
@@ -45,12 +45,12 @@ public class FileAnalyzeUtil {
                 switch (condition) {
                     case "equalLetters1":
                         if (!nextWord.equals("")) {
-                        if (nextWord.charAt(nextWord.length() - 1) == word.charAt(0)) {
-                            list.add(nextWord);
-                            list.add(word);
+                            if (nextWord.charAt(nextWord.length() - 1) == word.charAt(0)) {
+                                list.add(nextWord);
+                                list.add(word);
+                            }
                         }
-                    }
-                    nextWord = word;
+                        nextWord = word;
                         break;
                     case "numbers":
                         list.add(sortedNumbers(word));
@@ -64,7 +64,7 @@ public class FileAnalyzeUtil {
         return list;
     }
 
-    public static String sortedNumbers (String string) {
+    public static String sortedNumbers(String string) {
         List<String> list = Arrays.asList(string.split(" +"));
         List<Integer> numbersList = new ArrayList<>();
         for (String str : list)
@@ -97,4 +97,131 @@ public class FileAnalyzeUtil {
         return result.trim();
     }
 
+    public static List<String> listText(String path) {
+        String string = "";
+        List<String> list = new ArrayList<>();
+        String text = "";
+        try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
+            while ((string = reader.readLine()) != null) {
+                if (!string.equals("")) {
+                    text += string + " ";
+                }
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        Map<Character, Integer> charMap = new TreeMap<>();
+        char ch = ' ';
+        text = text.replace(" ", "");
+        text = text.toLowerCase();
+        while (!text.isEmpty()) {
+            ch = text.charAt(0);
+            text = text.substring(1);
+            if (charMap.containsKey(ch)) {
+                charMap.put(ch, charMap.get(ch) + 1);
+            } else charMap.put(ch, 1);
+        }
+        for (Map.Entry<Character, Integer> entry : charMap.entrySet()) {
+            list.add(entry.getKey() + "-" + entry.getValue());
+        }
+
+        return list;
+    }
+
+    public static List<String> wordsFrequency(String path) {
+        String string = "";
+        List<String> list = new ArrayList<>();
+        String text = "";
+        try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
+            while ((string = reader.readLine()) != null) {
+                if (!string.equals("")) {
+                    text += string + " ";
+                }
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        Map<String, Integer> stringMap = new TreeMap<>();
+        String[] wordsList = text.split(" +");
+        for (String word : wordsList) {
+            if (stringMap.containsKey(word)) {
+                stringMap.put(word, stringMap.get(word) + 1);
+            } else stringMap.put(word, 1);
+        }
+        int i = 0;
+        for (Map.Entry<String, Integer> entry : stringMap.entrySet()) {
+            wordsList[i++] = entry.getValue() + "-" + entry.getKey();
+        }
+        String[] result = Arrays.copyOf(wordsList, i);
+        Arrays.sort(result);
+        list = Arrays.asList(result);
+        return list;
+    }
+
+
+    public static List<String> sortNmbers(String path) {
+        String string = "";
+        List<String> list = new ArrayList<>();
+        String text = "";
+        try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
+            while ((string = reader.readLine()) != null) {
+                if (!string.equals("")) {
+                    text += string + " ";
+                }
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        String[] stringNumbers = text.split(" +");
+        int[] nums = new int[stringNumbers.length];
+        for (int j = 0; j < stringNumbers.length; j++) {
+            if (!stringNumbers[j].equals("")) {
+                nums[j] = Integer.parseInt(stringNumbers[j]);
+            }
+        }
+        Arrays.sort(nums);
+        try (FileWriter writer = new FileWriter(path)) {
+            for (int j = 0; j < stringNumbers.length; j++) {
+                stringNumbers[j] = "" + nums[j];
+                writer.write(stringNumbers[j] + System.lineSeparator());
+            }
+            writer.flush();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        list.addAll(Arrays.asList(stringNumbers));
+        return list;
+    }
+
+    public static double getProgress(String path) {
+        String string = "";
+        double sum = 0;
+        int num = 0;
+        try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
+            while ((string = reader.readLine()) != null) {
+                String[] data = string.split(",");
+                for (String element : data) {
+                    try {
+                        sum += Double.parseDouble(element);
+                        num++;
+                    } catch (NumberFormatException e) {
+
+                    }
+                }
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        return new BigDecimal(sum/num).setScale(2, RoundingMode.HALF_UP).doubleValue();
+    }
+
+
+
 }
+
+
+
+
+
+
+
--- src/test/java/com/rakovets/course/javabasics/practice/javaio/FileAnalyzeUtilTest.java
+++ src/test/java/com/rakovets/course/javabasics/practice/javaio/FileAnalyzeUtilTest.java
@@ -10,7 +10,10 @@ import java.util.List;
 public class FileAnalyzeUtilTest {
     String sep = File.separator;
     String textString = "src" + sep + "main" + sep + "resources" + sep + "strings.txt";
-    //String textNumbers = "src" + sep + "main" + sep + "resources" + sep + "numbers.txt";
+    String wordsTask = "src" + sep + "main" + sep + "resources" + sep + "words.txt";
+    String textNumbers = "src" + sep + "main" + sep + "resources" + sep + "numbers.txt";
+    String docSortNumbers = "src" + sep + "main" + sep + "resources" + sep + "sortNumbers.txt";
+    String docProgress = "src" + sep + "main" + sep + "resources" + sep + "progress.txt";
 
     @Test
     void Task2(){
@@ -31,12 +34,35 @@ public class FileAnalyzeUtilTest {
         Assertions.assertEquals(list, FileAnalyzeUtil.equalLetters(textString, "equalLetters1"));
     }
 
-
-    String textNumbers = "src" + sep + "main" + sep + "resources" + sep + "numbers.txt";
     @Test
     void Task5(){
         List<String> list = Arrays.asList("1 2 3", "67 68 69");
         Assertions.assertEquals(list, FileAnalyzeUtil.equalLetters(textNumbers, "numbers"));
     }
+
+    @Test
+    void Task6(){
+        List<String> list = Arrays.asList("a-2", "e-2", "h-3", "j-1", "l-2", "n-1", "o-3", "v-1");
+        Assertions.assertEquals(list, FileAnalyzeUtil.listText(textString));
+    }
+
+    @Test
+    void Task7(){
+        List<String> list = Arrays.asList("1-George", "1-Name", "1-Praha", "1-Yan", "1-born", "1-great", "1-in", "1-love", "1-name", "1-town!", "1-was", "2-Hello", "2-I", "2-Minsk", "2-My", "3-is");
+        Assertions.assertEquals(list, FileAnalyzeUtil.wordsFrequency(wordsTask));
+    }
+
+    @Test
+    void Task8() {
+        List<String> list = Arrays.asList("11", "22", "32", "33", "45", "67");
+        Assertions.assertEquals(list, FileAnalyzeUtil.sortNmbers(docSortNumbers));
+    }
+
+    @Test
+    void Task9() {
+        Assertions.assertEquals(6.83, FileAnalyzeUtil.getProgress(docProgress));
+    }
+
+
 
 }
